0c9d415ddad37d4f8a8574fbcef34d303e523c8c
Wong Shao Voon <shaovoon@yahoo.com>
2016-09-17 09:51:27 +0800

    Remove std:: prefix

--- Projects/PreAllocateRegex/BenchmarkRegex/BenchmarkRegex.cpp
+++ Projects/PreAllocateRegex/BenchmarkRegex/BenchmarkRegex.cpp
@@ -18,26 +18,28 @@
 #include <mutex>
 #include <memory> 
 
+using namespace std;
+
 class timer
 {
 public: 
 	timer() = default;
-	void start_timing(const std::string& text_)
+	void start_timing(const string& text_)
 	{
 		text = text_;
-		begin = std::chrono::high_resolution_clock::now();
+		begin = chrono::high_resolution_clock::now();
 	}
 	void stop_timing()
 	{
-		auto end = std::chrono::high_resolution_clock::now();
+		auto end = chrono::high_resolution_clock::now();
 		auto dur = end - begin;
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
-		std::cout << std::setw(35) << text << ":" << std::setw(5) << ms << "ms" << std::endl;
+		auto ms = chrono::duration_cast<chrono::milliseconds>(dur).count();
+		cout << setw(35) << text << ":" << setw(5) << ms << "ms" << endl;
 	}
 
 private:
-	std::string text;
-	std::chrono::steady_clock::time_point begin;
+	string text;
+	chrono::steady_clock::time_point begin;
 };
 
 #ifdef WIN32
@@ -58,71 +60,79 @@ static void do_not_optimize_away(void* p) {
 struct singleton
 {
 public:
-	typedef std::unordered_map<std::thread::id, std::unique_ptr<std::regex> > MapType;
-	static void set(const std::string& reg_exp) { s_reg_exp = reg_exp; }
-	static const std::regex& get()
+	typedef unordered_map<thread::id, unique_ptr<regex> > MapType;
+	static void set(const string& reg_exp) { s_reg_exp = reg_exp; }
+	static const regex& get()
 	{
-		std::lock_guard<std::mutex> lock(s_mutex);
+		lock_guard<mutex> lock(s_mutex);
 
-		std::thread::id thread_id = std::this_thread::get_id();
+		thread::id thread_id = this_thread::get_id();
 		MapType::iterator it = s_map.find(thread_id);
 		if (it == s_map.end())
 		{
-			std::unique_ptr<char[]> p = std::unique_ptr<char[]>(new char[1000]);
-			s_vec_temp_alloc.push_back(std::move(p));
-			s_map[thread_id] = std::unique_ptr<std::regex>(new std::regex(s_reg_exp));
+			prevent_false_sharing(1000);
+			s_map[thread_id] = unique_ptr<regex>(new regex(s_reg_exp));
 			return *s_map[thread_id];
 		}
 		return *(it->second);
 	}
 private:
+	static void prevent_false_sharing(size_t size)
+	{
+		unique_ptr<char[]> p = unique_ptr<char[]>(new char[size]);
+		s_vec_temp_alloc.push_back(move(p));
+	}
 	static MapType s_map;
-	static std::string s_reg_exp;
-	static std::mutex s_mutex;
-	static std::vector< std::unique_ptr<char[]> > s_vec_temp_alloc;
+	static string s_reg_exp;
+	static mutex s_mutex;
+	static vector< unique_ptr<char[]> > s_vec_temp_alloc;
 };
 singleton::MapType singleton::s_map;
-std::string singleton::s_reg_exp;
-std::mutex singleton::s_mutex;
-std::vector< std::unique_ptr<char[]> > singleton::s_vec_temp_alloc;
+string singleton::s_reg_exp;
+mutex singleton::s_mutex;
+vector< unique_ptr<char[]> > singleton::s_vec_temp_alloc;
 
 struct factory
 {
 public:
-	static std::unique_ptr<std::regex> get(const std::string& reg_exp)
+	static unique_ptr<regex> get(const string& reg_exp)
 	{
-		std::lock_guard<std::mutex> lock(s_mutex);
-		std::unique_ptr<char[]> p = std::unique_ptr<char[]>(new char[1000]);
-		s_vec_temp_alloc.push_back( std::move(p) );
-		return std::unique_ptr<std::regex>(new std::regex(reg_exp));
+		lock_guard<mutex> lock(s_mutex);
+		prevent_false_sharing(1000);
+		return unique_ptr<regex>(new regex(reg_exp));
 	}
 private:
-	static std::mutex s_mutex;
-	static std::vector< std::unique_ptr<char[]> > s_vec_temp_alloc;
+	static void prevent_false_sharing(size_t size)
+	{
+		unique_ptr<char[]> p = unique_ptr<char[]>(new char[size]);
+		s_vec_temp_alloc.push_back(move(p));
+	}
+	static mutex s_mutex;
+	static vector< unique_ptr<char[]> > s_vec_temp_alloc;
 };
-std::mutex factory::s_mutex;
-std::vector< std::unique_ptr<char[]> > factory::s_vec_temp_alloc;
+mutex factory::s_mutex;
+vector< unique_ptr<char[]> > factory::s_vec_temp_alloc;
 
-const std::string local_match(const std::string& text);
-const std::string static_match(const std::string& text); // not reentrant-safe
-const std::string singleton_match(const std::string& text);
-const std::string factory_match(const std::string& text, const std::regex& regex);
+const string local_match(const string& text);
+const string static_match(const string& text); // not reentrant-safe
+const string singleton_match(const string& text);
+const string factory_match(const string& text, const regex& regex);
 
-void parallel_invoke(int size, int threads, std::function<void(int, int)> func)
+void parallel_invoke(int size, int threads, function<void(int, int)> func)
 {
-	typedef std::unique_ptr<std::thread> PtrType;
-	std::vector< PtrType > vec;
+	typedef unique_ptr<thread> PtrType;
+	vector< PtrType > vec;
 	int each = size / threads;
 
 	for (int i = 0; i < threads; ++i)
 	{
 		if (i == threads - 1) // last thread
 		{
-			vec.emplace_back(PtrType(new std::thread(func, each*i, each*(i + 1) + (size % threads) - 1)));
+			vec.emplace_back(PtrType(new thread(func, each*i, each*(i + 1) + (size % threads) - 1)));
 		}
 		else
 		{
-			vec.emplace_back(PtrType(new std::thread(func, each*i, each*(i + 1) - 1 )));
+			vec.emplace_back(PtrType(new thread(func, each*i, each*(i + 1) - 1 )));
 		}
 	}
 
@@ -132,15 +142,15 @@ void parallel_invoke(int size, int threads, std::function<void(int, int)> func)
 	}
 }
 
-const std::string REG_EXP = ".*PRICE:.*US\\$(\\d+\\.\\d+|[-+]*\\d+).*PER SHARE";
+const string REG_EXP = ".*PRICE:.*US\\$(\\d+\\.\\d+|[-+]*\\d+).*PER SHARE";
 
 int main(int argc, char* argv[])
 {
 	const int LOOP = 1000000;
-	std::string str1 = "Zoomer PRICE: US$1.23 PER SHARE";
-	std::string str2 = "Boomer PRICE: US$4.56 PER SHARE";
+	string str1 = "Zoomer PRICE: US$1.23 PER SHARE";
+	string str2 = "Boomer PRICE: US$4.56 PER SHARE";
 	
-	std::vector<std::string> vec;
+	vector<string> vec;
 	vec.push_back(str1);
 	vec.push_back(str2);
 	
@@ -203,8 +213,8 @@ int main(int argc, char* argv[])
 
 	stopwatch.start_timing("factory regex object(4 threads)");
 	parallel_invoke(LOOP, 4, [&vec](int start, int end) {
-		std::unique_ptr<std::regex> ptr = factory::get(REG_EXP);
-		const std::regex& regex = *ptr;
+		unique_ptr<regex> ptr = factory::get(REG_EXP);
+		const regex& regex = *ptr;
 
 		for (int j = start; j < end; ++j)
 		{
@@ -217,62 +227,62 @@ int main(int argc, char* argv[])
 	stopwatch.stop_timing();
 	
 	/*
-	std::cout << local_match(str1) << std::endl;
-	std::cout << local_match(str2) << std::endl;
-	std::cout << static_match(str1) << std::endl;
-	std::cout << static_match(str2) << std::endl;
+	cout << local_match(str1) << endl;
+	cout << local_match(str2) << endl;
+	cout << static_match(str1) << endl;
+	cout << static_match(str2) << endl;
 	singleton::set(REG_EXP);
-	std::cout << singleton_match(str1) << std::endl;
-	std::cout << singleton_match(str2) << std::endl;
-	const std::regex regex(REG_EXP);
-	std::cout << factory_match(str1, regex) << std::endl;
-	std::cout << factory_match(str2, regex) << std::endl;
+	cout << singleton_match(str1) << endl;
+	cout << singleton_match(str2) << endl;
+	const regex regex(REG_EXP);
+	cout << factory_match(str1, regex) << endl;
+	cout << factory_match(str2, regex) << endl;
 	*/
 
 	return 0;
 }
 
-const std::string local_match(const std::string& text)
+const string local_match(const string& text)
 {
-	std::string ipo_price = "";
-	std::smatch what;
-	const std::regex regex(REG_EXP);
-	if (std::regex_match(text, what, regex))
+	string ipo_price = "";
+	smatch what;
+	const regex regex(REG_EXP);
+	if (regex_match(text, what, regex))
 	{
 		ipo_price = what[1];
 	}
 	return ipo_price;
 }
 
-const std::string static_match(const std::string& text) // not reentrant-safe
+const string static_match(const string& text) // not reentrant-safe
 {
-	std::string ipo_price = "";
-	std::smatch what;
-	static const std::regex regex(REG_EXP);
-	if (std::regex_match(text, what, regex))
+	string ipo_price = "";
+	smatch what;
+	static const regex regex(REG_EXP);
+	if (regex_match(text, what, regex))
 	{
 		ipo_price = what[1];
 	}
 	return ipo_price;
 }
 
-const std::string singleton_match(const std::string& text)
+const string singleton_match(const string& text)
 {
-	std::string ipo_price = "";
-	std::smatch what;
-	const std::regex& regex = singleton::get();
-	if (std::regex_match(text, what, regex))
+	string ipo_price = "";
+	smatch what;
+	const regex& regex = singleton::get();
+	if (regex_match(text, what, regex))
 	{
 		ipo_price = what[1];
 	}
 	return ipo_price;
 }
 
-const std::string factory_match(const std::string& text, const std::regex& regex)
+const string factory_match(const string& text, const regex& regex)
 {
-	std::string ipo_price = "";
-	std::smatch what;
-	if (std::regex_match(text, what, regex))
+	string ipo_price = "";
+	smatch what;
+	if (regex_match(text, what, regex))
 	{
 		ipo_price = what[1];
 	}
